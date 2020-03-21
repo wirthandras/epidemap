e3397e0bd31b1cffb9c64e1e95b2146169920f74
Andras Wirth <agrippa.wia@gmail.com>
2020-03-21 18:31:48 +0100

    Remove unused imports

--- src/main/java/hu/wirthandras/epidemap/contoller/IndexController.java
+++ src/main/java/hu/wirthandras/epidemap/contoller/IndexController.java
@@ -1,7 +1,5 @@
 package hu.wirthandras.epidemap.contoller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/hu/wirthandras/epidemap/domain/Disease.java
+++ src/main/java/hu/wirthandras/epidemap/domain/Disease.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Entity
 public class Disease {
